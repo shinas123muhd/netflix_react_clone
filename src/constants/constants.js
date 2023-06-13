6deb65a3d7dfe03f73dca01d7cc5e14eee79375e
Shinas Muhammed <shinasmuhd43@gmail.com>
2023-06-13 18:54:38 +0530

    first

---
+++ src/constants/constants.js
@@ -0,0 +1,3 @@
+export const baseUrl = 'https://api.themoviedb.org/3'
+export const API_KEY ='10e0038bb6ebe537b0393f92cfa131f9'
+export const imageUrl = 'https://image.tmdb.org/t/p/original'
---
+++ src/urls.js
@@ -0,0 +1,5 @@
+import {API_KEY} from './constants/constants'
+export const originals=`trending/all/week?api_key=${API_KEY}&language=en-US`
+export const action=`discover/movie?api_key=${API_KEY}&with_genres=28`
+export const horror=`discover/movie?api_key=${API_KEY}&with_genres=27`
+export const comedy=`discover/movie?api_key=${API_KEY}&with_genres=35`
